f1dd4d98ea7bef94985a0a86d11f94469adf84ce
Mahier Sydow <mahier.sydow8@gmail.com>
2021-07-21 18:09:57 +0900

    Long/Lat info section

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,65 +1,159 @@
-import React, { FC, useState } from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import { IconContext } from 'react-icons';
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
-import { SidebarData } from './SidebarData';
-import Submenu from './Submenu';
+import React, { FC } from "react";
 
-const Nav = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    height: 5rem;
-    background-color: white;
+import styled from "styled-components";
+import { IconContext } from "react-icons";
+import { GiGolfFlag } from "react-icons/gi";
+import { SidebarData } from "./SidebarData";
+import Submenu from "./Submenu";
+
+const SidebarNav = styled.div`
+  /* Rectangle 30 */
+  position: absolute;
+  width: 60px;
+  height: 954px;
+  left: 0px;
+  top: 0px;
+  z-index: 2;
+
+  background: #ffffff;
+  /* Elevation / Depth8 */
+
+  box-shadow: 0px 0.6px 1.8px rgba(0, 0, 0, 0.1),
+    0px 3.2px 7.2px rgba(0, 0, 0, 0.13);
 `;
 
-const SidebarNav = styled.div<{ sidebar: boolean }>`
-    width: 250px;
-    height: 100vh;
-    background-color: white;
-    position: fixed;
-    top: 0;
-    left: ${({ sidebar }) => (sidebar ? '0' : '-100%')};
-    transition: 350ms;
+const SidebarWrap = styled.div``;
+
+const SidebarSetion = styled.div`
+  width: 366px;
+  height: 100vh;
+  background-color: #fbfbfb;
+  position: fixed;
+  top: 0;
 `;
 
-const NavIcon = styled(Link)`
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    height: 5rem;
-    font-size: 2rem;
-    margin-left: 2rem;
+const SidebarParagraph = styled.div`
+  width: 306px;
+  height: 419px;
+  background-color: white;
+  color: black;
+  position: fixed;
+  top: 0;
+  margin-left: 60px;
 `;
 
-const SidebarWrap = styled.div`
+const SideInfoDiv = styled.div``;
+
+const InfoParagraph = styled.p`
+  width: 73px;
+  height: 22px;
+  margin-top: 10px;
+  margin-left: 17px;
+
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 15px;
+  line-height: 22px;
+  /* identical to box height */
+
+  color: #4d4c4c;
 `;
 
+const InfoDivLat = styled.div`
+  padding: 5px;
+  margin-top: 10px;
+`;
+
+const InfoLabelLat = styled.label`
+  width: 21px;
+  height: 19px;
+  left: 83px;
+  top: 62px;
+  margin-left: 20px;
+
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 13px;
+  line-height: 19px;
+  /* identical to box height */
+
+  color: #4d4c4c;
+`;
+
+const InfoLabelLong = styled.label`
+  width: 22px;
+  height: 19px;
+  left: 83px;
+  top: 62px;
+  margin-left: 22px;
+
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 13px;
+  line-height: 19px;
+  /* identical to box height */
+
+  color: #4d4c4c;
+`;
+
+const InfoInput = styled.input`
+  width: 147px;
+  height: 33px;
+  left: 120px;
+  top: 54px;
+
+  border: 0.5px solid #c7c7c7;
+  box-sizing: border-box;
+  border-radius: 5px;
+
+  margin-left: 20px;
+`;
+
+const HorizontalDivider = styled.hr`
+  width: 306px;
+  height: 0px;
+  left: 60px;
+  top: 278px;
+
+  border: 1px solid #f4f4f4;
+`;
 
 const Sidebar: FC = () => {
-    const [sidebar, setSidebar] = useState(false);
-    const showSidebar = () => setSidebar(!sidebar);
-
-    return (
-        <IconContext.Provider value={{ color: '#C7C7C7'}}>
-            <Nav>
-                <NavIcon to="#" onClick={showSidebar}>
-                    <AiOutlineMenu />
-                </NavIcon>
-            </Nav>
-            <SidebarNav sidebar={sidebar}>
-                <SidebarWrap>
-                    <NavIcon to="#" onClick={showSidebar}>
-                        <AiOutlineClose />
-                    </NavIcon>
-                    {SidebarData.map((item, index) => {
-                        return <Submenu item={item} key={index} />;
-                    })}
-                </SidebarWrap>
-            </SidebarNav>
-        </IconContext.Provider>
-    );
+  return (
+    <IconContext.Provider value={{ color: "#C7C7C7" }}>
+      <SidebarSetion>
+        <SidebarNav>
+          <SidebarWrap>
+            {SidebarData.map((item, index) => {
+              return <Submenu item={item} key={index} />;
+            })}
+          </SidebarWrap>
+        </SidebarNav>
+        <SidebarParagraph>
+          <SideInfoDiv>
+            <InfoParagraph>
+              <GiGolfFlag /> Location
+            </InfoParagraph>
+            <form>
+              <InfoDivLat>
+                <InfoLabelLat>Lat</InfoLabelLat>
+                <InfoInput></InfoInput>
+              </InfoDivLat>
+              <div>
+                <InfoLabelLong>Lon</InfoLabelLong>
+                <InfoInput></InfoInput>
+              </div>
+            </form>
+            <br />
+            <HorizontalDivider />
+          </SideInfoDiv>
+        </SidebarParagraph>
+      </SidebarSetion>
+    </IconContext.Provider>
+  );
 };
 
 export default Sidebar;
--- src/components/Submenu.tsx
+++ src/components/Submenu.tsx
@@ -1,52 +1,53 @@
-import React, { FC, useState } from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import { SidebarItem } from '../models/SidebarItem';
+import React, { FC, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { SidebarItem } from "../models/SidebarItem";
 
 type SidebarLinkProps = {
-    item: SidebarItem;
+  item: SidebarItem;
 };
 
 const SidebarLink = styled(Link)`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 3.75rem;
-    width: 15px;
-    font-size: 1.125rem;
-    padding: 2rem;
-    text-decoration: none;
-    color: #ffffff;
-    &:hover {
-        cursor: pointer;
-    }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 3.75rem;
+  width: 15px;
+  font-size: 1.125rem;
+  padding: 2rem;
+  text-decoration: none;
+  color: #ffffff;
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
-const SidebarLabel = styled.span`
-
-`;
+const SidebarLabel = styled.span``;
 
 const IconOuter = styled.span`
-    background-color: #1F4782;
-    border-radius: 5px;
-    padding: 10px;
+  background-color: #1f4782;
+  border-radius: 5px;
+  padding: 10px;
+  width: 44px;
+  height: 44px;
+  left: 8px;
+  top: 8px;
 `;
 
 const Submenu: FC<SidebarLinkProps> = ({ item }) => {
-    const [subnav, setSubnav] = useState(false);
-    const showSubnav = () => setSubnav(!subnav);
+  const [subnav, setSubnav] = useState(false);
+  const showSubnav = () => setSubnav(!subnav);
 
-    return (
-        <>
-            <SidebarLink to={item.path} onClick={showSubnav}>
-                <IconOuter>
-                    {item.icon}
-                    <SidebarLabel>{item.title}</SidebarLabel>
-                </IconOuter>
-            </SidebarLink>
-        </>
-    );
+  return (
+    <>
+      <SidebarLink to={item.path} onClick={showSubnav}>
+        <IconOuter>
+          {item.icon}
+          <SidebarLabel>{item.title}</SidebarLabel>
+        </IconOuter>
+      </SidebarLink>
+    </>
+  );
 };
 
 export default Submenu;
-
